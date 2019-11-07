6d8e968b09161f5e02ee55b5f1bbbaeac3819b2e
Matt Reeves <Chairman@Reeves.lan1>
2019-11-06 22:42:23 -0500

    insertionSort

--- SRC/Main.cpp
+++ SRC/Main.cpp
@@ -6,8 +6,19 @@
 using namespace std; 
 
 int main(){
-	selectionSort *ss = new selectionSort();
-	//vector<int> *vec = new vector<int>();
-	//ss->sort(*&vec);
+	vector<int> *vec = new vector<int>(); 
+	vector<int>::iterator it;
+	
+	vec->push_back(4);
+	vec->push_back(3);
+	vec->push_back(1);
+	vec->push_back(2);
+	vec->push_back(5);
+	
+	insertionSort *is = new insertionSort();
+	is->sort(*vec);
+	
+	free(vec);
+	free(is);
 	return 0;
 }
--- SRC/insertionSort.h
+++ SRC/insertionSort.h
@@ -5,9 +5,32 @@ class insertionSort : public ISort{
 	insertionSort(){}
 	~insertionSort(){}
 	void sort(vector<int> & vector);
+	
+	
+	
+	void printAll(vector<int> &vec){
+		for(int i=0; i< vec.size(); i++){
+			cout << vec[i] << endl;
+		};
+		cout << endl << endl << endl;
+	}
 
 };
 
-void insertionSort::sort(vector<int> & vector){
-	cout << "insertionSort" << endl;
+void insertionSort::sort(vector<int> &vec){
+	for(int i=0; i < vec.size() ; i++){
+		int primeNode = i;
+		int swapNode = i-1;
+		while(vec[primeNode] < vec[swapNode] && swapNode!=-1){
+			cout << "swapNode " << swapNode << endl;
+			cout << "primeNode " << primeNode << endl;
+			int tempNode = vec[swapNode];
+			vec[swapNode] = vec[primeNode];
+			vec[primeNode] = tempNode;
+			primeNode = primeNode-1;
+			swapNode = swapNode-1;
+		}
+	}
+	
+	printAll(vec);
 }
