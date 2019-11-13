aa0e78ae93ec3ffd5d2834e2ff1db9e2d13c6cd2
Matt Reeves <Chairman@Reeves.lan1>
2019-11-12 21:29:11 -0500

    selectionSort

--- SRC/Main.cpp
+++ SRC/Main.cpp
@@ -7,18 +7,23 @@ using namespace std;
 
 int main(){
 	vector<int> *vec = new vector<int>(); 
-	vector<int>::iterator it;
-	
+
+	vec->push_back(16);
+	vec->push_back(8);
+	vec->push_back(20);
+	vec->push_back(23);
 	vec->push_back(4);
 	vec->push_back(3);
 	vec->push_back(1);
 	vec->push_back(2);
 	vec->push_back(5);
+	vec->push_back(0);
 	
-	insertionSort *is = new insertionSort();
-	is->sort(*vec);
+	
+	selectionSort *ss = new selectionSort();
+	ss->sort(*vec);
 	
 	free(vec);
-	free(is);
+	free(ss);
 	return 0;
 }
--- SRC/selectionSort.h
+++ SRC/selectionSort.h
@@ -5,9 +5,37 @@ class selectionSort : public ISort{
 	selectionSort(){}
 	~selectionSort(){}
 	void sort(vector<int> & vector);
+	
+	void printAll(vector<int> &vec){
+		for(int i=0; i< vec.size(); i++){
+			cout << vec[i] << endl;
+		};
+		cout << endl << endl << endl;
+	}
 
 };
 
 void selectionSort::sort(vector<int> & vector){
-	cout << "insertionSort" << endl;
+	int currentMin = 0;
+	int currentItem = 0;
+	int sorted = 0;
+	int temp = 0;
+	
+	while(true){
+		currentMin = sorted;
+		for(int i = sorted ; i<vector.size(); i++){
+			if(vector[currentMin] > vector[i]){
+				currentMin = i;
+			}
+		}
+		temp = vector[sorted];
+		vector[sorted] = vector[currentMin];
+		vector[currentMin] = temp;
+		sorted = sorted+1;
+		if(sorted == vector.size()-1){
+			break;
+		}
+	}
+	
+	printAll(vector);
 }
